e30ca539c06a18b862d4e7bd46aca739d64b4310
Barry <semieng94@gmail.com>
2020-01-26 21:38:05 -0600

    done with 1-3

--- SQL Homework Solved.sql
+++ SQL Homework Solved.sql
@@ -3,9 +3,7 @@ use sakila;
 select * from actor;
 SELECT a.first_name, a.last_name from actor a;
 -- 1b
-ALTER TABLE actor
-ADD COLUMN `Actor Name` VARCHAR(50);
-SELECT CONCAT (first_name," ", last_name) FROM actor;
+SELECT CONCAT(first_name, " ", last_name) FROM actor AS `Actor Name`;
 -- 2a
 SELECT actor_id, first_name, last_name FROM actor
 WHERE first_name = 'Joe';
@@ -13,6 +11,15 @@ WHERE first_name = 'Joe';
 SELECT actor_id, first_name, last_name FROM actor
 WHERE last_name LIKE "%GEN%";
 -- 2c
-
-
-
+SELECT actor_id, first_name, last_name FROM actor
+WHERE last_name LIKE "%LI%"
+ORDER BY last_name, first_name ASC;
+-- 2d
+SELECT * FROM country
+WHERE country IN ("Afghanistan","China","Bangladesh");
+-- 3a
+ALTER TABLE actor
+ADD `description` BLOB;
+-- 3b
+ALTER TABLE actor
+DROP `description`;
